8a9846c461a6922f773fa0b76ad06be0270fe255
Patrick <patrickmhaney@gmail.com>
2025-09-05 14:00:12 +0000

    updates

---
+++ send_text.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $(readlink -f ${0}))
+. ${BASEDIR}/picks.cfg
+
+
+#Ben
+curl -X POST https://textbelt.com/text \
+  --data-urlencode phone='5136467655' \
+  --data-urlencode message="The picks are in! 
+-${Ben}
+-${Leland}
+-${Pat}
+-${BD}
+-${Hank}
+http://www.ShedParlay.life" \
+  -d key=9b9bf9d43b8103c904e702c11257fed6c6ba3de48TV04t4ld2xpudpNMukLme8N6
+#Leland
+curl -X POST https://textbelt.com/text \
+  --data-urlencode phone='5132388370' \
+  --data-urlencode message="The picks are in! 
+-${Ben}
+-${Leland}
+-${Pat}
+-${BD}
+-${Hank}
+http://www.ShedParlay.life" \
+  -d key=9b9bf9d43b8103c904e702c11257fed6c6ba3de48TV04t4ld2xpudpNMukLme8N6
+#Pat
+curl -X POST https://textbelt.com/text \
+  --data-urlencode phone='5139108729' \
+  --data-urlencode message="The picks are in! 
+-${Ben}
+-${Leland}
+-${Pat}
+-${BD}
+-${Hank}
+http://www.ShedParlay.life" \
+  -d key=9b9bf9d43b8103c904e702c11257fed6c6ba3de48TV04t4ld2xpudpNMukLme8N6
+#BD
+curl -X POST https://textbelt.com/text \
+  --data-urlencode phone='5132604063' \
+  --data-urlencode message="The picks are in! 
+-${Ben}
+-${Leland}
+-${Pat}
+-${BD}
+-${Hank}
+http://www.ShedParlay.life" \
+  -d key=9b9bf9d43b8103c904e702c11257fed6c6ba3de48TV04t4ld2xpudpNMukLme8N6
+#Hank
+curl -X POST https://textbelt.com/text \
+  --data-urlencode phone='5135151331' \
+  --data-urlencode message="The picks are in! 
+-${Ben}
+-${Leland}
+-${Pat}
+-${BD}
+-${Hank}
+http://www.ShedParlay.life" \
+  -d key=9b9bf9d43b8103c904e702c11257fed6c6ba3de48TV04t4ld2xpudpNMukLme8N6
